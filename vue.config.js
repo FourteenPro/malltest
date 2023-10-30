268603d737999e2cdf2fb50a5f9a9f1a17b29255
fourteen <fourteen7x@163.com>
2023-10-30 16:31:36 +0800

    创建配置文件vue.config.js配置别名、.editorconfig设置代码风格

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets':'@/aassets',
+        'components':'@/components',
+        'network':'@/network',
+        'views':'@/views',
+      }
+    }
+  }
 })
