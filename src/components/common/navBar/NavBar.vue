b70f499fbe20d6e11f20dd0c562f638d71561062
fourteen <fourteen7x@163.com>
2023-10-31 10:33:10 +0800

    导航栏的封装、在首页使用

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav-tab">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'navBar',
+    }
+</script>
+
+<style>
+    .nav-tab {
+        display: flex;
+        text-align: center;
+        height: 44px;
+        line-height: 44px;
+        box-shadow: 0 1px 1px rgba(100,100,100,.1);
+    }
+    .nav-tab .left,.nav-tab .right {
+        width: 60px;
+    }
+    .nav-tab .center {
+        flex: 1;
+    }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>首页</div>
+    <nav-bar class="home-nav">
+        <div slot="center">购物街</div>
+    </nav-bar>
 </template>
 
 <script>
+    import NavBar from 'components/common/navBar/NavBar' 
     export default{
         name: 'home',
+        components:{
+            NavBar
+        }
 
     }
 </script>
 
 <style>
-
+    .home-nav { 
+        background-color: #ff8e96;
+        color: #fff;
+    }
 </style>
